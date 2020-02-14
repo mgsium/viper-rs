2e5959fb6c7cc6d1a3d35b18b9206b4ece241368
Musab Guma'a <musabgumaa@gmail.com>
2020-02-14 06:26:15 +0000

    Added Project Naming Feature

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 extern crate clap;
 
-use clap::{Arg, App};
-//use std::env;
+use clap::{Arg, App, SubCommand};
+
+use std::fs;
+use std::error::Error;
+use std::path;
 
 fn main() {
 
     let matches = App::new("viper")
                     .version("0.1")
                     .author("Musab G. <musabgumaa@gmail.com>")
-                    .arg(Arg::with_name("venv")
-                        .short("v")
-                        .long("venv")
+                    .arg(Arg::with_name("env")
+                        .short("e")
+                        .long("env")
                         .help("Creates a venv for the project."))
                     .arg(Arg::with_name("dependencies")
                         .short("D")
@@ -18,16 +21,29 @@ fn main() {
                         .help("Specify Dependencies for the project.")
                         .multiple(true)
                         .takes_value(true))
+                    .subcommand(SubCommand::with_name("new")
+                        .about("Creates a new project.")
+                        .arg(Arg::with_name("name")
+                            .short("n")
+                            .long("name")
+                            .help("Specify the name for the project")
+                            .required(true)
+                            .index(1))
+                    )
                     .get_matches();
-
-    /*
-    let output = if let values = matches.values_of("dependencies") {
-        for value in values.unwrap() {
-            println!("{:?}", value);
-        }
-    };
-    */
-
-    let _dependencies: Vec<&str> = matches.values_of("dependencies").unwrap().collect();
     
+    let ProjectName = matches.subcommand_matches("new").unwrap().value_of("name").unwrap();
+    let PathName = format!("./{}", ProjectName);
+    println!("{:?}", ProjectName);
+
+    fs::create_dir_all(&PathName);
+
+    let mut filePath = format!("{}/main.py", PathName);
+    let path = path::Path::new(&filePath);
+    let display = path.display();
+
+    let mut file = match fs::File::create(&path) {
+        Err(why) => panic!("Couldn't create {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
 }
